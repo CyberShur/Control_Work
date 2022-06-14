076b634a3a446e2c6b37ff655daebb597c423e1c
Aleksandr <aldrobakha@gmail.com>
2022-06-14 22:33:44 +0300

    task solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите количество проверяемых элементов: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+string[] inputArray = new string[length];
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+    Console.Write($"Введите {i+1}-й элемент: ");
+    inputArray[i] = Console.ReadLine();
+}
+
+string[] resultArray = new string[inputArray.Length];
+
+Console.WriteLine();
+Console.WriteLine("Входящий массив: ");
+PrintArray(inputArray);
+Console.WriteLine();
+
+ResultArray (inputArray, resultArray);
+
+Console.WriteLine("Итоговый массив: ");
+PrintArray(resultArray);
+
+void ResultArray (string[] array, string[] result)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
